451cc302c48faacc22fe62bdc292f566f09cd246
xota <joek@xota.nl>
2018-05-23 23:53:47 +0200

    background() voór de tekenfuncties

--- tekening.js
+++ tekening.js
@@ -5,14 +5,15 @@ function setup() {
 }
     	
 function draw() {
+	background(achtergrond);
+	achtergrond += 1;
+	achtergrond %= 255; //rest modulo 255 om waarde tussen 0 en 255 te krijgen
+
 	noFill();
 	rect(50, 50, 200, 200);
 	fill(255, 0,0); //rood
 	rect(60, 60, 180, 180);
 	fill(0,0,255); //blauwellipse(200,200,100,50);
         
-	background(achtergrond);
-	achtergrond += 1;
-	achtergrond %= 255; //rest modulo 255 om waarde tussen 0 en 255 te krijgen
 }
     
