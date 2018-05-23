6345dc36c557580db4215b05b177483219f14155
xota <joek@xota.nl>
2018-05-23 23:28:42 +0200

    Een paar basisvormen

---
+++ tekening.js
@@ -0,0 +1,14 @@
+function setup() {
+        createCanvas(400, 400);
+        background(220);
+}
+    	
+function draw() {
+          noFill();
+          rect(50, 50, 200, 200);
+          fill(255, 0,0); //rood
+          rect(60, 60, 180, 180);
+          fill(0,0,255); //blauw
+          ellips(200,200,100,50);
+}
+    
