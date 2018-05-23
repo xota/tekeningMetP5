71198efa8726ae992649c30f09b8ad9b099d55f9
xota <joek@xota.nl>
2018-05-23 23:58:01 +0200

    typo, return ingevoegd

--- tekening.js
+++ tekening.js
@@ -13,7 +13,8 @@ function draw() {
 	rect(50, 50, 200, 200);
 	fill(255, 0,0); //rood
 	rect(60, 60, 180, 180);
-	fill(0,0,255); //blauwellipse(200,200,100,50);
+	fill(0,0,255); //blauw
+	ellipse(200,200,100,50);
         
 }
     
